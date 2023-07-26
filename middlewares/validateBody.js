4517bde55b4b4e495afc2c135ead4c9faf63781e
Roman Vdovichenko <vrn6870@gmail.com>
2023-07-26 05:58:40 +0300

    new2

---
+++ middlewares/validateBody.js
@@ -0,0 +1,31 @@
+const { HttpError } = require('../helpers');
+
+const validateBody = schema => {
+    const func = (req, res, next) => {
+        const { name, email, phone } = req.body;
+        let value = '';
+        if (!name && !email && !phone) {
+            next(HttpError(400, 'missing fields'));
+        }
+        if (!name) {
+            value = 'name';
+        }
+        if (!email) {
+            value = 'email';
+        }
+        if (!phone) {
+            value = 'phone';
+        }
+        if (value) {
+            next(HttpError(400, `missing required ${value} field`))
+        }
+        const { error } = schema.validate(req.body);
+        if (error) {
+            next(HttpError(400, error.message));
+        }
+        next();
+    }
+    return func;
+}
+
+module.exports = validateBody;
