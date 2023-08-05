fce53f8f9729d10a81e21dc881a8a21c400d69ca
Roman Vdovichenko <vrn6870@gmail.com>
2023-08-05 05:59:15 +0300

    new4

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,7 +6,7 @@ const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 5} = req.query;
   const skip = (page - 1) * limit;
-    const result = await Contact.find({owner}, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'email subscription');
+    const result = await Contact.find({owner}, '-createdAt -updatedAt', {skip, limit: Number(limit)}).populate('owner', 'email subscription');
     res.status(200).json(result);
 }
 
